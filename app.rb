44deda74e730b67c8ac09687bcce49f9c6528ac9
Maggie Summers <summers.maggie@gmail.com>
2018-02-21 09:50:18 -0800

    add patch to app.rb

--- app.rb
+++ app.rb
@@ -47,3 +47,10 @@ get('/lists/:id/edit') do
   @list = List.find(params.fetch("id").to_i())
   erb(:list_edit)
 end
+
+patch("/lists/:id") do
+  name = params.fetch("name")
+  @list = List.find(params.fetch("id").to_i())
+  @list.update({:name => name})
+  erb(:list)
+end
